2598b9ceb948b835de1d87cc0c1c233fb9a539f2
KA-rg <krishjain260905@gmail.com>
2025-07-18 19:55:50 +0530

    made template model

--- app.js
+++ app.js
@@ -2,17 +2,19 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
+const Listing = require("./models/listing");
+const { title } = require("process");
 
 main()
 .then(() => {
-  console.log("connection successful");
+  console.log("connection to DB");
 })
 .catch((err) => {
   console.log(err);
 })
 
 async function main() {
-  await mongoose.connect("mongodb://localhost:27017/whatsapp");
+  await mongoose.connect("mongodb://localhost:27017/wanderlust");
 }
 
 app.set("views", path.join(__dirname,"views"));
@@ -24,9 +26,18 @@ app.get("/", (req,res) => {
   console.log("server is working");
 });
 
-
+app.get("/testListing", async (req, res) =>{
+  let sampleListing = new Listing({
+    title: "My New Villa",
+    description: "By the beach",
+    price: 1200,
+    location: "Calangute, Goa",
+    country: "India",
+  });
+  res.send("successful testing");
+});
 
 port = 8080;
 app.listen(port, () => {
   console.log(`app is listening to port:${port}`);
-})
+});
---
+++ models/listing.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema ({
+  title:{ 
+    type: String,
+    require: true,
+  },
+  description:{
+    type: String,
+  },
+  image: {
+    type: String,
+    set: (v) =>
+      v === ""
+        ? "https://unsplash.com/photos/a-person-standing-in-a-narrow-canyon-between-two-mountains-lrhF4w-KKjA"
+         : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
