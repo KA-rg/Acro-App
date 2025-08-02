6a1fb596975b71e27d4f19ab34cb2f96000555e2
KA-rg <krishjain260905@gmail.com>
2025-08-02 18:18:46 +0530

    Create route prepared

--- app.js
+++ app.js
@@ -28,11 +28,23 @@ app.get("/listings", async (req,res) => {
   res.render("listings/index", { allListing });
 });
 
+//new route
+app.get("/listings/new", (req,res) =>{ 
+  res.render("listings/new.ejs");
+});
+
 //show route
 app.get("/listings/:id", async (req,res) =>{
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/show.ejs", { listing });
+});
+
+//Create Route
+app.post("/listings", async (req,res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
 });
 
 app.get("/", (req,res) => {
