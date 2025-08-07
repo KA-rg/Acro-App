2c24094eb573ddda1fa35fa5875439b59527af51
KA-rg <krishjain260905@gmail.com>
2025-08-07 22:20:20 +0530

    wrapAsync and expressErrors added to app.js

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const Listing = require("./models/listing");
 const { title } = require("process");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressErrors = require("./utils/ExpressErrors.js");
 
 main()
 .then(() => {
@@ -27,65 +29,77 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 
 //index route
-app.get("/listings", async (req,res) => {
+app.get("/listings", wrapAsync(async (req,res,next) => {
   const allListing = await Listing.find({});
   res.render("listings/index", { allListing });
-});
+}));
 
 //new route
 app.get("/listings/new", (req,res) =>{ 
-  res.render("listings/new.ejs");
+  res.render("listings/new");
 });
 
 //show route
-app.get("/listings/:id", async (req,res) =>{
+app.get("/listings/:id", wrapAsync(async (req,res,next) =>{
   let { id } = req.params;
   const listing = await Listing.findById(id);
-  res.render("listings/show.ejs", { listing });
-});
+  res.render("listings/show", { listing });
+}));
 
 //Create Route
-app.post("/listings", async (req,res) => {
+app.post("/listings", wrapAsync(async (req,res,next) => {
   const newListing = new Listing(req.body.listing);
   await newListing.save();
   res.redirect("/listings");
-});
+}));
 
 //edit route
-app.get("/listing/:id/edit", async (req,res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req,res,next) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
-  res.render("listings/edit.ejs", { listing });
-});
+  res.render("listings/edit", { listing });
+}));
 
 //update route
-app.put("/listing/:id", async (req,res) =>{
+app.put("/listings/:id", wrapAsync(async (req,res,next) =>{
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, {...req.body.listing});
   res.redirect(`/listings/${id}`);
-});
+}));
 
 //delete route
-app.delete("/listing/:id", async (req,res) => {
+app.delete("/listings/:id", wrapAsync(async (req,res,next) => {
   let { id } = req.params;
   let deletedListing = await Listing.findByIdAndDelete(id);
   res.redirect("/listings");
-});
-
-app.get("/", (req,res) => {
-  console.log("server is working");
-});
+}));
 
 // app.get("/testListing", async (req, res) =>{
-//   let sampleListing = new Listing({
-//     title: "My New Villa",
-//     description: "By the beach",
-//     price: 1200,
-//     location: "Calangute, Goa",
-//     country: "India",
-//   });
-//   res.send("successful testing");
+  //   let sampleListing = new Listing({
+    //     title: "My New Villa",
+    //     description: "By the beach",
+    //     price: 1200,
+    //     location: "Calangute, Goa",
+    //     country: "India",
+    //   });
+    //   res.send("successful testing");
+    // });
+    
+   // 1. All routes
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+// 2. Catch-all for undefined routes
+// app.all("*", (req, res, next) => {
+//   next(new ExpressErrors(404, "Page not found"));
 // });
+
+// 3. Error handler at the very end
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Something went wrong" } = err;
+  res.status(status).render("listings/error", { err });
+});
 
 port = 8080;
 app.listen(port, () => {
