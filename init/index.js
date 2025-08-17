2dc1a0076849c9cc4c623d52b69eeb48ba6263fa
KA-rg <krishjain260905@gmail.com>
2025-08-17 19:08:21 +0530

    signup direct to logi feature,owner feature enabled

--- init/index.js
+++ init/index.js
@@ -17,6 +17,10 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "68a17d8afdf656c4a2663c81",
+  }));
   await Listing.insertMany(initData.data);
   console.log("data was initialised");
 };
--- routes/user.js
+++ routes/user.js
@@ -1,19 +1,9 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressErrors = require("../utils/ExpressErrors.js");
 const User = require("../models/user.js");
 const passport = require("passport");
-
-const validateUser = (req, res, next) => {
-  let { error } = userSchema.validate(req.body);
-  if(error) {
-    let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressErrors(400, errMsg);  //server side validation
-  } else {
-    next();
-  }
-};
+const { saveRedirectUrl } = require("../middleware.js");
 
 router.get("/signup", (req,res) => {
   res.render("users/signup.ejs");
@@ -24,9 +14,14 @@ router.post("/signup", wrapAsync (async (req, res) => {
       let { username, email, password } = req.body;
       const newUser = new User({ email, username });
       const registeredUser = await User.register(newUser, password);
-      console.log(registeredUser);
-      req.flash("success", "Welcome to Wanderlust!!");
+      
+      req.login(registeredUser, (err) => {
+      if (err) {
+        return next(err);
+      }
+      req.flash("success", "Welcome to Wanderlust!");
       res.redirect("/listings");
+    })
     } catch (e) {
       req.flash("error", e.message);
       res.redirect("/signup");
@@ -39,13 +34,15 @@ router.get("/login", (req,res) => {
 });
 
 router.post("/login",
+  saveRedirectUrl,
   passport.authenticate("local", {
     failureRedirect: "/login",
     failureFlash: true,
   }),
   async(req,res) => {
     req.flash("success", "Welcome back to wanderlust!");
-    res.redirect("/listings");
+    let redirectUrl = res.locals.redirect || "/listings";
+    res.redirect(redirectUrl);
   });
 
 router.get("/logout", (req, res) => {
